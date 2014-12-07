f7a6f28141fbe10cb24aaad43e03c8569c1982ca
kamal24 <kmlgrg2425@gmail.com>
2014-12-07 11:36:18 -0800

    Webpages

---
+++ index.php
@@ -0,0 +1,9 @@
+
+<html>
+//code for header is in header.php
+<?php include_once('WebPages/Header.php'); ?>
+
+//code for body
+<?php include_once('WebPages/body.php') ?>
+<?php include_once('WebPages/footer.php') ?>
+</html>
